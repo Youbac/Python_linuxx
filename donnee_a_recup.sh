c10cb6a29e8f3f743342b7b51c1f5609d869b582
EC2 Default User <ec2-user@ip-172-31-40-140.eu-west-3.compute.internal>
2023-03-26 00:12:27 +0000

    Projet Amelioré

--- donnee_a_recup.sh
+++ donnee_a_recup.sh
@@ -1,2 +1,5 @@
 curl https://fr.investing.com/equities/apple-computer-inc > /home/ec2-user/Projet_Youbac_Linux/Bloc_notes_donnee.txt
-cat /home/ec2-user/Projet_Youbac_Linux/Bloc_notes_donnee.txt | grep -oP '(?<="instrument-price-last">)[^<]+' | tr ',' '.' >> /home/ec2-user/Projet_Youbac_Linux/prix_stock_recup.csv
+price=$(cat /home/ec2-user/Projet_Youbac_Linux/Bloc_notes_donnee.txt | grep -oP '(?<="instrument-price-last">)[^<]+' | tr ',' '.')
+dataprix=$(date +"%Y-%m-%d %T")
+
+echo "$price,$dataprix" >> /home/ec2-user/Projet_Youbac_Linux/prix_stock_recup.csv
